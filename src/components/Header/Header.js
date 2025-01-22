926abccb1fafa5a350b0b420172d5c9f95e3c897
Omari Nyarko <omarinyarko@MacBookPro.lan>
2025-01-22 08:12:49 -0500

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>BigOrders</h1>
+      <h2>Rain Albany Restaurant</h2>
+    </header>
+  );
+};
+
+export default Header;
